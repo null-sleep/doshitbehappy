8c26c11f952d0f15ba63cd9f038e3548dc192002
Dhruv Jauhar <dhruvjhr@gmail.com>
2025-08-23 18:48:52 -0400

    basic datastructure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+mod heatmap {
+    
+    #[derive(Debug, Clone, PartialEq)]
+    pub struct Heatmap {
+        pub name: String,
+        pub data: Vec<Vec<DataPoint>>,
+        pub metadata: Option<Metadata>,
+    }
+    
+    #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+    pub struct DataPoint {
+        pub date: u64,
+        pub value: f64,
+    }
+    
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    pub struct Metadata {
+        pub rows: usize,
+        pub cols: usize,
+        pub row_label: Option<String>,
+        pub col_label: Option<String>,
+    }
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
